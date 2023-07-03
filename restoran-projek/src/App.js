c81401d9b2e96c20bde13d8ea3c2c84996f2b7de
Tarissazzhr <tarissa1402@gmail.com>
2023-07-03 10:20:03 +0700

    satu

--- restoran-projek/src/App.js
+++ restoran-projek/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./view/home";
+import Menu from "./view/menu";
+import About from "./view/about";
+import Contact from "./view/contact";
+import _header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <_header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ restoran-projek/src/view/menu.jsx
@@ -0,0 +1,11 @@
+function menu() {
+    return (
+      <div className="m-11 p-10 bg-gray-200 rounded-xl">
+        <p className="text-center text-3xl font-bold">Hei!</p>
+        <p className="text-center text-gray-400 pt-5">Halo</p>
+      </div>
+    );
+  }
+  
+  export default menu;
+  
